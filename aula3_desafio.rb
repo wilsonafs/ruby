3f96735acab35bcc965a4947437f038ef05bd00b
Wilson <wilsonafs@hotmail.com>
2019-01-18 17:19:59 -0200

    melhorando lógica da calculadora

--- aula3_desafio.rb
+++ aula3_desafio.rb
@@ -1,43 +1,43 @@
 puts "Iniciando a calculadora"
 
 loop do 
-    puts "Selecione uma das operações abaixo: "
-    puts "\n"
+    puts "Selecione uma das operações abaixo: \n\n "
     puts "1 - Adição"
     puts "2 - Subtração"
     puts "3 - Multiplicação"
     puts "4 - Divisão"
-    puts "0 - Sair do programa."
-    puts "\n"
+    puts "0 - Sair do programa.\n\n"
+    
     operador = gets.strip.to_i
     system "clear"
 
-    if operador == 0
-        break
-    end
+    break if operador == 0
 
-    puts "\n"
-    puts "Insira o primeiro valor: "
-    num1 = gets.strip.to_i
-    puts "\n"
-    puts "Agora insira o segundo valor:"
-    num2 = gets.strip.to_i
+    if[1, 2, 3, 4].include?(operador)
+        puts "\n"
+        puts "Insira o primeiro valor: "
+        num1 = gets.strip.to_i
+        puts "\n"
+        puts "Agora insira o segundo valor:"
+        num2 = gets.strip.to_i
+    end
     case operador 
 
     when 1
         resultado = num1 + num2
-        operacao = "soma"
     when 2
         resultado = num1 - num2
-        operacao = "subtracao"
     when 3
         resultado = num1 * num2
-        operacao = "multiplicacao"
     when 4
         resultado = num1 / num2
-        operacao = "divisao"
+    else 
+        resultado = "Opção inválida"
     end
     
-    puts "\n"
-    puts "O resultado da #{operacao} de #{num1} e #{num2} é #{resultado}"
+    if resultado == "Opção inválida"
+        puts resultado
+    else
+        puts "O resultado é #{resultado}"
+    end
 end
